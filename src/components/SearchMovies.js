cf56f5099bc39da48baddc85c7fddff60a4ca040
rjiovan <rjiovan@protonmail.com>
2020-09-19 20:01:37 -0400

    add dotenv

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -2,9 +2,16 @@ import React from 'react'
 import '../components/SearchMovies.css';
 
 export default function SearchMovies() {
+
+    const searchMovies = async (e, query) => {
+        e.preventDefault();
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    }
+
     return ( 
         <div>
-            <form className="form">
+            <form className="form" onSubmit={searchMovies}>
                 <label className="label" htmlFor="query"></label>
                 <input 
                     className="input" 
