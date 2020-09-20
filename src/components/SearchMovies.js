7e638b1612be5f3daa9a4a069fece2998f2ebaad
rjiovan <rjiovan@protonmail.com>
2020-09-19 20:28:03 -0400

    fetch movie info

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -3,10 +3,18 @@ import '../components/SearchMovies.css';
 
 export default function SearchMovies() {
 
-    const searchMovies = async (e, query) => {
+    const searchMovies = async (e) => {
         e.preventDefault();
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        const query = 'project power'
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API}&language=en-US&query=${query}&page=1&include_adult=false`;
 
+        try {
+            const res = await fetch(url); 
+            const data = await res.json();
+            console.log(data)
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return ( 
